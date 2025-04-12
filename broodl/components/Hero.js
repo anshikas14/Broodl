0165e85d6dbfd0fe463513670e6010ceb0dbdd7e
Anshika <anshika.ip.21@nitj.ac.in>
2025-04-12 23:52:58 +0530

    added calendar import

--- broodl/components/Hero.js
+++ broodl/components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Fugaz_One } from 'next/font/google';
 import Button from './Button';
+import Calendar from './Calendar';
 const fugaz = Fugaz_One({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -19,6 +20,7 @@ export default function Hero() {
         <Button text="Sign Up" />
         <Button text="Login" dark/>
        </div>
+       <Calendar/>
     </div>
   )
 }
