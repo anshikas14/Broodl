94c9f81828453c819eda7b18499211cdc7d7370c
Anshika <anshika.ip.21@nitj.ac.in>
2025-04-13 03:25:44 +0530

    added moods section

--- broodl/app/dashboard/page.js
+++ broodl/app/dashboard/page.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function DashboardPage() {
 
-    const isAuthenticated = false
+    const isAuthenticated = true
 
     let children = (
         <Login/>
